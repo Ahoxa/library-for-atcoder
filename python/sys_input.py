ea6bda78273923cc98797d5c7ace9320e371859b
hayashibara <yujinyuto1223@gmail.com>
2023-11-17 16:18:06 +0900

    add sys_input

---
+++ python/sys_input.py
@@ -0,0 +1,8 @@
+import sys
+
+n = int(sys.stdin.readline().rstrip())
+a = list(map(int, sys.stdin.readline().split()))
+
+# sys.stdin.readline()でインプットを受け取り、rstrip()は改行を検知する
+# １変数のみの入力受け取り ... .rstrip()
+# ２変数、もしくはリストでの入力受け取り　... .split()
